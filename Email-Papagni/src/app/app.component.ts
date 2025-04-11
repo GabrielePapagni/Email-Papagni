988a29095a73d57a9658f6c4345566b31dbd976a
Gabriele Papagni <gabriele.papagni07@gmail.com>
2025-04-11 08:47:07 +0000

    Quinto commit

--- Email-Papagni/src/app/app.component.ts
+++ Email-Papagni/src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Email} from "./sent-mail/email.model";
+import {SentMailComponent} from "./sent-mail/sent-mail.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SentMailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  addEmail(titolo: HTMLInputElement, messaggio: HTMLInputElement): boolean {
-    let t = titolo.value;
-    let v = messaggio.value;
-    console.log(`Aggiungendo il titolo: `+t+ ` e il messaggio: `+ v);
-    console.log(`Aggiungendo il titolo: `+titolo.value+ ` e il messaggio: `+ messaggio.value);
-    console.log(`Aggiungendo il titolo: ${titolo.value} e il messaggio: ${messaggio.value}`);
-    return false;
+  title = 'Manda una Email';
+  emails:Email[]
+
+  constructor(){
+    this.emails = [
+      new Email("laguda@gmail.com", "Mail inutile", "Ciao!"),
+      new Email("papagni@gmail.com", "Mail importante", "Buonasera!"),
+      new Email("malizia@gmail.com", "KENSHIRO", "KENSHIRO")
+    ]
+  }
+
+  aggiungiMail(email: HTMLInputElement, oggetto: HTMLInputElement, testo: HTMLInputElement){
+    this.emails.push(new Email(email.value, oggetto.value, testo.value))
+    console.log("elemento aggiunto")
+    email.value = ""
+    oggetto.value = ""
+    testo.value = ""
   }
 }
